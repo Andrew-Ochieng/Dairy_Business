90dfb301709bbea173ed167a2628f05333a3b446
AndrewMartial <andrewomosh44@gmail.com>
2022-07-16 15:17:50 +0300

    add milk output weekly reports

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ let hour = newDate.getHours()
 greetings = () => {
     if (hour < 12) {
         time.innerText = 'Morning'
-    } else if (hour > 12 && hour < 18) {
+    } else if (hour < 17) {
         time.innerText = 'Afternoon'
     } else {
         time.innerText = 'Evening'
@@ -21,7 +21,14 @@ greetings()
 
 
 //inputs
+//initialize variables
+const shadeNum = document.getElementById('shade-num')
+const milkAmount = document.getElementById('milk-amount')
 const inputs = document.getElementById('form-inputs');
+const postAmount = document.getElementById('post-amount')
+const shadeOutput = document.getElementById('shade-output')
+const milkOutput = document.getElementById('milk-output')
+const amountOutput = document.getElementById('amount-output')
 
 inputs.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -31,8 +38,30 @@ inputs.addEventListener('submit', (e) => {
 
 
 let submitInputs = () => {
-    // console.log('hey the code worked :)')
+
+    if (shadeNum != '' && milkAmount != '') {
+        // creating new element and adding value to it
+        let newShadeNum = document.createElement('LI')
+        newShadeNum.appendChild(document.createTextNode(shadeNum.value))
+
+        let newMilkAmount = document.createElement('LI')
+        newMilkAmount.appendChild(document.createTextNode(milkAmount.value))
     
+        // adding li to ul
+        shadeOutput.appendChild(newShadeNum)
+
+        milkOutput.appendChild(newMilkAmount)
+
+    } else {
+        console.log('inputs cannot be empty')
+    }
+
+    //reset input boxes
+    shadeNum.value = '';
+    milkAmount.value = '';
+
+
+
 }
 
 
