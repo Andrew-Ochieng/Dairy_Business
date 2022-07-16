015b3b4a7d26ced4e55939940c6c92cf32605f84
AndrewMartial <andrewomosh44@gmail.com>
2022-07-16 08:52:53 +0300

    add greetings function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,44 @@
-// 
+// set greetings depending on time of day
+const time = document.getElementById('time')
+
+const newDate = new Date()
+
+let hour = newDate.getHours()
+
+greetings = () => {
+    if (hour < 12) {
+        time.innerText = 'Morning'
+    } else if (hour > 12 && hour < 18) {
+        time.innerText = 'Afternoon'
+    } else {
+        time.innerText = 'Evening'
+    }
+}
+
+greetings()
+
+
+
+
+//arrays 
+let shades = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
